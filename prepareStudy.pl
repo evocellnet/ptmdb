f821d21bbd74280a9604d8ec6ddf6b7f87cdd3ac
David Ochoa <ochoa@ebi.ac.uk>
2013-11-12 12:21:04 +0000

    This change add flexibility to use the ptmdb as module

--- prepareStudy.pl
+++ prepareStudy.pl
@@ -1,23 +1,24 @@
 use warnings;
 use strict;
 
-my $configfile = $ARGV[0];
-my $idformat = $ARGV[1];
-my $taxid = $ARGV[2];
-my $infile = $ARGV[3];
-my $colnum = $ARGV[4];
-my $fs = $ARGV[5];
-my $headerBool = $ARGV[6];
-my $idCol = $ARGV[7];
-my $modificationTypeCol = $ARGV[8];
-my $modificationTypeAll = $ARGV[9];
-my $aaCol = $ARGV[10];
-my $resnumCol = $ARGV[11];
-my $locScoreCol = $ARGV[12];
-my $peptideCol = $ARGV[13];
-my $peptideScoredCol = $ARGV[14];
-my $conditionalData = $ARGV[15];
-my $spectralCount = $ARGV[16];
+my $ptmdbDir = $ARGV[0];
+my $configfile = $ARGV[1];
+my $idformat = $ARGV[2];
+my $taxid = $ARGV[3];
+my $infile = $ARGV[4];
+my $colnum = $ARGV[5];
+my $fs = $ARGV[6];
+my $headerBool = $ARGV[7];
+my $idCol = $ARGV[8];
+my $modificationTypeCol = $ARGV[9];
+my $modificationTypeAll = $ARGV[10];
+my $aaCol = $ARGV[11];
+my $resnumCol = $ARGV[12];
+my $locScoreCol = $ARGV[13];
+my $peptideCol = $ARGV[14];
+my $peptideScoredCol = $ARGV[15];
+my $conditionalData = $ARGV[16];
+my $spectralCount = $ARGV[17];
 
 #In case it's a non-quantitative experiment
 my $spectralCountsCol;
@@ -26,28 +27,28 @@ my $spectralCountsCol;
 my @conditionalHeaders;
 
 if($conditionalData eq "true"){
-	for (my $i=17;$i<scalar(@ARGV);$i++){
+	for (my $i=18;$i<scalar(@ARGV);$i++){
 		my $thiscolname =$ARGV[$i];
 		push(@conditionalHeaders, $thiscolname);
 	}
 }else{
-	$spectralCountsCol = $ARGV[17];
+	$spectralCountsCol = $ARGV[18];
 }
 
 #PREFORMATING 
-my $preformatScript = "./src/insertingPTMs/preformatPTMs.pl";
+my $preformatScript = $ptmdbDir."/src/insertingPTMs/preformatPTMs.pl";
 my $preformatCommand = "perl ".$preformatScript;
-for (my $i=3;$i<scalar(@ARGV);$i++){
+for (my $i=4;$i<scalar(@ARGV);$i++){
 	$preformatCommand.=" ";
 	$preformatCommand.=("\'".$ARGV[$i]."\'");
 }
 
 #MAPPING IDs SCRIPT
-my $mappingScript = "./src/insertingPTMs/mappingIDs.R";
+my $mappingScript = $ptmdbDir."/src/insertingPTMs/mappingIDs.R";
 my $mappingCommand = "Rscript ".$mappingScript." ".$configfile." ".$idformat." ".$taxid;
 
 #PEPTIDE MATCH
-my $peptideMatchScript = "./src/insertingPTMs/peptideMatcher.pl";
+my $peptideMatchScript = $ptmdbDir."/src/insertingPTMs/peptideMatcher.pl";
 my $peptideCommand = "perl ".$peptideMatchScript;
 
 system($preformatCommand." | ".$mappingCommand. " | ".$peptideCommand);
