df1e261bb8054a72c444c51f974193ffb59c96a8
David Ochoa <ochoa@ebi.ac.uk>
2013-11-18 11:44:01 +0000

    Now the modifications are only inserted at the begining and not as many times as the number of organisms

---
+++ src/databaseXreferences/insertModifications.pl
@@ -0,0 +1,41 @@
+use warnings;
+use strict;
+use DBI;
+
+#Database information
+my $dbhost=$ARGV[0];
+my $database=$ARGV[1];
+my $dbuser=$ARGV[2];
+my $dbpass=$ARGV[3];
+my $MODIFICATIONS_FILE = $ARGV[4];
+
+#Connecting to the database
+my $dbh = DBI->connect('DBI:mysql:'.$database.";".$dbhost, $dbuser, $dbpass, {AutoCommit => 0}) || die "Could not connect to database: $DBI::errstr";
+my $errflag=0;
+
+### INSERTING MODIFICATION TYPES #################################################################################
+
+open(MODFILE, $MODIFICATIONS_FILE);
+my @modfilelines = <MODFILE>;
+close(MODFILE);
+
+foreach my $line (@modfilelines){
+	chomp($line);
+	my @fields = split("\t", $line);
+	my $insModificationsStatement =  $dbh->prepare('INSERT INTO modification(id,description) VALUES (?,?)');
+	print $fields[0]."\n";
+	unless($insModificationsStatement->execute($fields[0], $fields[1])){
+		$errflag=1;
+	}
+}
+
+### FINISHING #################################
+if($errflag){
+    my $error = DBI->errstr;
+    $dbh->rollback();
+	$dbh->disconnect();
+    die "could not insert rows: $error\n";
+}
+#$dbh->rollback();
+
+$dbh->commit();
