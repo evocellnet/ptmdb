f2afa6faa3848914b96a3f528863169b4736209b
David Ochoa <ochoa@ebi.ac.uk>
2013-12-18 13:30:27 +0000

    Help improved and ordered publications

--- src/ptmdbR/src/R/getAllPublications.R
+++ src/ptmdbR/src/R/getAllPublications.R
@@ -1,6 +1,6 @@
-#' Connects to ptmdb database
-#'
 #' Gets the list of publications on ptmdb
+#'
+#' The publications available in a given database are listed ordered by publication date
 #'
 #' @param db An object of the class \code{\link{MySQLConnection}} 
 #' @export
@@ -10,6 +10,6 @@
 #' @return Produces a dataframe containing each of the publications in the database
 
 getAllPublications <- function(db){
-	publications <- dbGetQuery(db, "SELECT * FROM publication ORDER DESC")
+	publications <- dbGetQuery(db, "SELECT * FROM publication ORDER BY publication_date DESC")
 	return(publications)
 }
