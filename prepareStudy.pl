ebd3d4c8336fcb3506c650ddc41fd03009bb2a1b
David Ochoa <ochoa@ebi.ac.uk>
2014-01-10 18:09:52 +0000

    config.sh is no longer accessed directly by any other script outside the root directory
    The scripts accessing it directly are insertOrganisms.sh and
    createDatabase.sh. Both should be included as part of the makefile.

--- prepareStudy.pl
+++ prepareStudy.pl
@@ -3,33 +3,41 @@ use strict;
 
 my $ptmdbDir = $ARGV[0];
 my $configfile = $ARGV[1];
-my $idformat = $ARGV[2];
-my $taxid = $ARGV[3];
-my $infile = $ARGV[4];
-my $colnum = $ARGV[5];
-my $fs = $ARGV[6];
-my $headerBool = $ARGV[7];
-my $idCol = $ARGV[8];
-my $modificationTypeCol = $ARGV[9];
-my $modificationTypeAll = $ARGV[10];
-my $aaCol = $ARGV[11];
-my $resnumCol = $ARGV[12];
-my $locScoreCol = $ARGV[13];
-my $peptideCol = $ARGV[14];
-my $peptideFormat = $ARGV[15];
-my $peptideScoredCol = $ARGV[16];
-my $conditionalData = $ARGV[17];
-my $spectralCountsCol = $ARGV[18];
+my $dbhost= $ARGV[2];
+my $database=$ARGV[3]
+my $dbuser=$ARGV[4]
+my $dbpass=$ARGV[5]
+my $dbport=$ARGV[6]
+my $idformat = $ARGV[7];
+my $taxid = $ARGV[8];
+my $infile = $ARGV[9];
+my $colnum = $ARGV[10];
+my $fs = $ARGV[11];
+my $headerBool = $ARGV[12];
+my $idCol = $ARGV[13];
+my $modificationTypeCol = $ARGV[14];
+my $modificationTypeAll = $ARGV[15];
+my $aaCol = $ARGV[16];
+my $resnumCol = $ARGV[17];
+my $locScoreCol = $ARGV[18];
+my $peptideCol = $ARGV[19];
+my $peptideFormat = $ARGV[20];
+my $peptideScoredCol = $ARGV[21];
+my $conditionalData = $ARGV[22];
+my $spectralCountsCol = $ARGV[23];
 
 #In case it's a conditional experiment
 my @conditionalHeaders;
 
 if($conditionalData eq "true"){
-	for (my $i=19;$i<scalar(@ARGV);$i++){
+	for (my $i=24;$i<scalar(@ARGV);$i++){
 		my $thiscolname =$ARGV[$i];
 		push(@conditionalHeaders, $thiscolname);
 	}
 }
+
+
+
 
 #PREFORMATING 
 my $preformatScript = $ptmdbDir."/src/insertingPTMs/preformatPTMs.pl";
@@ -41,7 +49,8 @@ for (my $i=4;$i<scalar(@ARGV);$i++){
 
 #MAPPING IDs SCRIPT
 my $mappingScript = $ptmdbDir."/src/insertingPTMs/mappingIDs.R";
-my $mappingCommand = "Rscript ".$mappingScript." ".$configfile." ".$idformat." ".$taxid;
+my $mappingCommand = "Rscript ".$mappingScript." ".$dbhost." ".$database." ".$dbuser." ".$dbpass." ".$dbport." ".$idformat." ".$taxid;
+
 
 #PEPTIDE MATCH
 my $peptideMatchScript = $ptmdbDir."/src/insertingPTMs/peptideMatcher.pl";
