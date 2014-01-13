cac881e57452b73190347d93039cc131375c4f07
David Ochoa <ochoa@ebi.ac.uk>
2014-01-13 09:46:51 +0000

    config.sh argument removed

--- prepareStudy.pl
+++ prepareStudy.pl
@@ -2,35 +2,34 @@ use warnings;
 use strict;
 
 my $ptmdbDir = $ARGV[0];
-my $configfile = $ARGV[1];
-my $dbhost= $ARGV[2];
-my $database=$ARGV[3]
-my $dbuser=$ARGV[4]
-my $dbpass=$ARGV[5]
-my $dbport=$ARGV[6]
-my $idformat = $ARGV[7];
-my $taxid = $ARGV[8];
-my $infile = $ARGV[9];
-my $colnum = $ARGV[10];
-my $fs = $ARGV[11];
-my $headerBool = $ARGV[12];
-my $idCol = $ARGV[13];
-my $modificationTypeCol = $ARGV[14];
-my $modificationTypeAll = $ARGV[15];
-my $aaCol = $ARGV[16];
-my $resnumCol = $ARGV[17];
-my $locScoreCol = $ARGV[18];
-my $peptideCol = $ARGV[19];
-my $peptideFormat = $ARGV[20];
-my $peptideScoredCol = $ARGV[21];
-my $conditionalData = $ARGV[22];
-my $spectralCountsCol = $ARGV[23];
+my $dbhost= $ARGV[1];
+my $database=$ARGV[2]
+my $dbuser=$ARGV[3]
+my $dbpass=$ARGV[4]
+my $dbport=$ARGV[5]
+my $idformat = $ARGV[6];
+my $taxid = $ARGV[7];
+my $infile = $ARGV[8];
+my $colnum = $ARGV[9];
+my $fs = $ARGV[10];
+my $headerBool = $ARGV[11];
+my $idCol = $ARGV[12];
+my $modificationTypeCol = $ARGV[13];
+my $modificationTypeAll = $ARGV[14];
+my $aaCol = $ARGV[15];
+my $resnumCol = $ARGV[16];
+my $locScoreCol = $ARGV[17];
+my $peptideCol = $ARGV[18];
+my $peptideFormat = $ARGV[19];
+my $peptideScoredCol = $ARGV[20];
+my $conditionalData = $ARGV[21];
+my $spectralCountsCol = $ARGV[22];
 
 #In case it's a conditional experiment
 my @conditionalHeaders;
 
 if($conditionalData eq "true"){
-	for (my $i=24;$i<scalar(@ARGV);$i++){
+	for (my $i=23;$i<scalar(@ARGV);$i++){
 		my $thiscolname =$ARGV[$i];
 		push(@conditionalHeaders, $thiscolname);
 	}
